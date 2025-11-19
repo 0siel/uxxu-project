624393f5b0facf0c33d93a4606d13a2b6a45d30f
0siel <osiel.rubio.suarez@gmail.com>
2025-11-18 21:29:38 -0600

    Cookie localhost fix

--- src/auth/constants/jwt.constants.ts
+++ src/auth/constants/jwt.constants.ts
@@ -1,2 +1,3 @@
 export const JWT_KEY = "2RFLSK*&788@@#$44RFHHDJHDJS/+";
 export const EXPIRES_IN = "250s";
+export const TOKEN_NAME = "auth_fot_uxxu_token";
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: ["http://127.0.0.1:3000", "http://localhost:3000"], // <--- Check this line carefully
+      origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       credentials: true,
     },
   });
+
+  app.use(cookieParser());
 
   const config = new DocumentBuilder()
     .setTitle("UxxU Project")
